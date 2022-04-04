cc56217546c5f3c388208c452cff20d3b406f6a6
Jennifer Moore <jenniferplusplus@gmail.com>
2022-04-04 14:57:56 -0500

    using es6 modules and scoped variables

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRoutes from './routes/index.js'
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRoutes);
 
-module.exports = app;
+export default app;
