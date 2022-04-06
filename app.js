3c52040318f78bfe819c97245db0f62c2df92ff3
Jennifer Moore <jenniferplusplus@gmail.com>
2022-04-05 21:59:18 -0500

    done

--- app.js
+++ app.js
@@ -9,13 +9,11 @@ import auth from './middleware/auth.js'
 
 
 const app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(auth);
 
-app.use('/api', tripRoutes, vehicleRoutes, driverRoutes);
+app.use('/api', auth, tripRoutes, vehicleRoutes, driverRoutes);
 
 app.use(notFound);
 app.use(authError);
